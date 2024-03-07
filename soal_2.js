a80fe894e14376df9699cc50fb32b9a631595c0d
Enardo Stefanus <stefanus.enardo05@gmail.com>
2024-03-07 14:35:03 +0700

    Enardo Stefanus - 535230072

--- soal_2.js
+++ soal_2.js
@@ -1,26 +1,33 @@
 const students = [
-	{
-  	id: "535250001",
-    score: 98.0
-  },
-  {
-  	id: "535250001",
-    score: 45.0
-  },
-  {
-  	id: "535250001",
-    score: 97.75
-  },
-  {
-  	id: "535250001",
-    score: 99.25
-  },
-  {
-  	id: "535250001",
-    score: 78.5
-  },
-];
-
-students.sort((a, b) => b.score - a.score);
-
-console.log(students);
+    {
+      id: "535250001",
+      score: 98.0
+    },
+    {
+      id: "535250002",
+      score: 45.0
+    },
+    {
+      id: "535250003",
+      score: 97.75
+    },
+    {
+      id: "535250004",
+      score: 99.25
+    },
+    {
+      id: "535250005",
+      score: 78.5
+    },
+  ];
+  
+  students.sort((a, b) => {
+    if (a.score > b.score) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  
+  console.log(students);
+  
